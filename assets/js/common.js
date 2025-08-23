26ba85e2648626b1cc3b2cd12688275a945b7d72
Scarmonit <Scarmonit@gmail.com>
2025-08-23 05:03:27 -0400

    1

--- assets/js/common.js
+++
@@ -1,9 +0,0 @@
-// Common JavaScript functions
-
-// Show notification message
-function showMessage(message, type = 'info') {
-    // ...message display logic...
-}
-
-// Other common functions
-// ...
